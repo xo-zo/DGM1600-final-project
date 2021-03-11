bdfe254bb86e85b519311d8bb68773670eb2d1db
xo-zo <zoecalcote@gmail.com>
2021-03-11 00:15:02 -0700

    Displayed all planet names on planets.html

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import * as planets from './planets.js'
+import { planets } from './planets.js'
 
 var allPlanets = planets
-console.log(allPlanets);
+
+for (var i = 0; i < allPlanets.length; i++) {
+    // create a new element of type div as a variable
+    var div = document.createElement('div');
+
+    // assign the new div a class of 'planet' -> use CSS for styling later
+    div.className = 'planet';
+
+    // fill the div with a <p> element and fill the planet name
+    div.innerHTML = '<p>' + allPlanets[i].name + '</p>';
+
+    // add the whole div variable to the document body
+    document.body.appendChild(div);
+
+    // iterate and go to the next planet
+}
